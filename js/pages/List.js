5a56278c890cd1837f2bb937120da7f1deffb924
TheDethronerCZ <brilliant2023.hotmail@gmail.com>
2025-08-17 19:14:47 +0200

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -77,7 +77,7 @@ export default {
                     </table>
                 </div>
                 <div v-else class="level" style="height: 100%; justify-content: center; align-items: center;">
-                    <p>(ノಠ益ಠ)ノ彡┻━┻</p>
+                    <p>No work lol</p>
                 </div>
             </div>
             <div class="meta-container">
