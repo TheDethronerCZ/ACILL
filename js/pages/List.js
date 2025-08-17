3161c4ac5d8f3e7c8e0fc7ba2ac4be523d24c079
TheDethronerCZ <brilliant2023.hotmail@gmail.com>
2025-08-17 19:18:27 +0200

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -100,19 +100,19 @@ export default {
                     </template>
                     <h3>Submission Requirements</h3>
                     <p>
-                        Achieved the record without using hacks (however, FPS bypass is allowed, up to 360fps)
+                        Submit levels using the "submit record" button
                     </p>
                     <p>
-                        Achieved the record on the level that is listed on the site - please check the level ID before you submit a record
+                        Showcase must be on the level that you claim to be
                     </p>
                     <p>
-                        Have either source audio or clicks/taps in the video. Edited audio only does not count
+                        Showcase should include fake clicks, doesnt have to tho
                     </p>
                     <p>
-                        The recording must have a previous attempt and entire death animation shown before the completion, unless the completion is on the first attempt. Everyplay records are exempt from this
+                        Showcase should have some sort of way to tell its botted
                     </p>
                     <p>
-                        The recording must also show the player hit the endwall, or the completion will be invalidated.
+                        The Showcase must show the whole level
                     </p>
                     <p>
                         Do not use secret routes or bug routes
@@ -121,7 +121,7 @@ export default {
                         Do not use easy modes, only a record of the unmodified level qualifies
                     </p>
                     <p>
-                        Once a level falls onto the Legacy List, we accept records for it for 24 hours after it falls off, then afterwards we never accept records for said level
+                        Levels are ranked by @qwsasz not @thedethronergd
                     </p>
                 </div>
             </div>
